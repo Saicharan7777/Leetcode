35eff154f26d58b07f4b1c71d7bd1555e7dcc6c1
Saicharan Maddimsetti <147791999+Saicharan7777@users.noreply.github.com>
2026-04-05 23:28:11 +0530

    Time: 24 ms (87.69%) | Memory: 59 MB (57.24%) - LeetSync

--- 4273-mirror-frequency-distance/mirror-frequency-distance.cpp
+++ 4273-mirror-frequency-distance/mirror-frequency-distance.cpp
@@ -2,29 +2,28 @@ class Solution {
 public:
     int mirrorFrequency(string s) {
         int n=s.size();
-        unordered_map<int,int>r;
+        int r[128]={0};
         for(int i=0;i<n;i++)
         {
             r[s[i]]++;
         }
 
         int sum=0;
-        for(int i=0;i<n;i++)
-        {
-            char m;
-            if(r[s[i]]>0)
-            {
-              if(isalpha(s[i]))
-              {
-                m='a'+'z'-s[i];
-              }
-              else
-              {
-                m='0'+'9'-s[i];
-              }
-              sum+=abs(r[s[i]]-r[m]);
-              r[s[i]]=0;
-              r[m]=0;
+        for(char i='a';i<='z';i++) {
+            char m='a'+'z'-i;
+            if(r[i]>0||r[i]>0){
+                sum+=abs(r[i]-r[m]);
+                r[i]=0;
+                r[m]=0;
+            }
+        }
+
+        for(char i='0';i<='9';i++) {
+            char m='0'+'9'-i;
+            if(r[i]>0||r[m]>0){
+                sum+=abs(r[i]-r[m]);
+                r[i]=0;
+                r[m]=0;
             }
         }
         return sum;
