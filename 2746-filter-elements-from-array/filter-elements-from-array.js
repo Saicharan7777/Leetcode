2bc966a75c4711d0592ef8a61957ca5bd512818a
Saicharan Maddimsetti <147791999+Saicharan7777@users.noreply.github.com>
2026-05-15 23:02:26 +0530

    Time: 48 ms (30.41%) | Memory: 54 MB (26.23%) - LeetSync

---
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let r=[];
+    for(let i=0;i<arr.length;i++)
+    {
+        if(fn(arr[i],i))r.push(arr[i]);
+    }
+    return r;
+};
