c5ed6cad010e436c97346cff468c2923f7b63548
Saicharan Maddimsetti <147791999+Saicharan7777@users.noreply.github.com>
2026-06-09 21:06:16 +0530

    Time: 4 ms (11.18%) | Memory: 25.9 MB (46.75%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
@@ -5,9 +5,9 @@ public:
        
      while(i<n&&j<n)
      {
-        if(nums[i]%2==0)i+=2;
-        else if(nums[j]%2==1)j+=2;
-        else swap(nums[i],nums[j]);
+        while(i<n&&nums[i]%2==0)i+=2;
+        while(j<n&&nums[j]%2==1)j+=2;
+        if(i<n&&j<n)swap(nums[i],nums[j]);
       }
         return nums;
     }
