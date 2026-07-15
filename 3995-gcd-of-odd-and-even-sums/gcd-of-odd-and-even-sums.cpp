9d8ad8e15026ad97eb976dafc3bc326a8569d1ff
Saicharan Maddimsetti <147791999+Saicharan7777@users.noreply.github.com>
2026-07-15 21:47:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (85.02%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,33 +1,6 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int s=0,v=0;
-        int i=0,o=0,j=1;
-        while(i<n&&o<n)
-        {
-            if(j%2==0)
-            {
-                s+=j;
-                i++;
-            }
-            else
-            {
-                v+=j;
-                o++;
-            }
-            j++;
-        }
-
-        int max1 = max(v, s);
-        int min1 = min(v, s);
-
-        while(min1 != 0)
-        {
-            int temp = min1;
-            min1 = max1 % min1;
-            max1 = temp;
-        }
-
-        return max1;
+        return n;
     }
 };
