b052606a745c7e5fa290a1f280b870a07df7da58
Saicharan Maddimsetti <147791999+Saicharan7777@users.noreply.github.com>
2026-07-18 22:22:07 +0530

    Time: 3 ms (26.93%) | Memory: 9.3 MB (82.44%) - LeetSync

--- 4355-rearrange-string-to-avoid-character-pair/rearrange-string-to-avoid-character-pair.cpp
+++ 4355-rearrange-string-to-avoid-character-pair/rearrange-string-to-avoid-character-pair.cpp
@@ -2,26 +2,13 @@ class Solution {
 public:
     string rearrangeString(string s, char x, char y) {
         int n = s.size(), c = 0, v = 0;
-        for(int i = 0; i < n; i++)
-            {
-                if(s[i] == x)c++;
-                else if(s[i] == y)v++;
-            }
-
-        if(c == 0 || v == 0) return s;
-
-        int i = 0, j = n - 1;
-        while(i < j)
-            {
-                if (s[i] == x && s[j] == y)
-                {
-                    swap(s[i], s[j]);
-                    i++, j--;
-                }
-                else if(s[i] != x) i++;
-                else if(s[j] != y) j--;
-            }
-
-        return s;
+        string str = "";
+        for(int i = 0; i < n; i++) {
+            if(s[i] == y) str += y;
+        }
+        for(int i = 0; i < n; i++) {
+            if(s[i] != y) str += s[i];
+        }
+        return str;
     }
 };
